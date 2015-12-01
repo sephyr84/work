1796281f02d66e7964b11748d7d10afcce0098fe
sephyr84 <sephyr84@gmail.com>
2015-12-01 19:31:42 +0900

    Add TimeData_Coordi

---
+++ publicData/TimeData_Coordi.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import unicodedata
+import time
+import csv
+import sys
+from datetime import datetime, timedelta
+
+#date define
+dt = datetime.today()
+td = timedelta(days = -1)
+basedate = dt + td
+basedate = basedate.date()
+base_date = basedate.strftime("20%y%m%d")
+
+x_range = 149
+y_range = 253
+
+#API parameters
+EndpointUrl_ForecastTimeData = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService/ForecastTimeData'
+ServiceKey = 'ssyoBDTCSe4RHaioipEsxKEecLycCh5MI4b5JWZ4swH9e1kAo%2BfLKpg0Rf3V26ISwnwfQQ7qG5T066pQIu%2BK9w%3D%3D'
+
+#sample Variables
+base_time = '0630'
+
+csv_file = open('TimeData_Coordi.dat', "w")
+cw = csv.writer(csv_file, delimiter = ',', quotechar = '|')
+
+for i in range(1, x_range):
+	for j in range(1, y_range):
+		nx = str(i)
+		ny = str(j)
+		REST = '&base_date=' + base_date + '&base_time=' + base_time + '&nx=' + nx + '&ny=' + ny
+		URL = EndpointUrl_ForecastTimeData + '?ServiceKey=' + ServiceKey + REST +'&_type=json'
+		r = requests.get(URL)
+
+		rawData = json.loads(r.content)		
+		totalCount = rawData['response']['body']['totalCount']
+		baseData = rawData['response']['body']['items']['item']
+		category = unicodedata.normalize('NFKD', baseData[1]['category']).encode('ascii','ignore')
+		fcstValue = str(baseData[1]['fcstValue'])
+		
+		if category == 'PTY':
+			if fcstValue != '-1':
+				cw.writerow([nx, ny])
+
