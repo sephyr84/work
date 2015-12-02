9ecee51169c3ea9542a8e2141450470e676059fc
sephyr84 <sephyr84@gmail.com>
2015-12-02 09:28:05 +0900

    Add Table_Create

--- Hive/Table_Create.sql
+++ Hive/Table_Create.sql
@@ -3,9 +3,10 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS bdc_5.cjk_mart_usr_hdfs (
 	user_name		STRING,
 	type			STRING,
+	schema			STRING,
 	table_name		STRING,
-	SIZE			INT)
-PARTITIONED BY (etl_ym	STRING)
+	SIZE			INT,
+	etl_ym			STRING)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 #사용자 폴더(/user/b10144880)에 새로운 폴더를 생성(hdfs_user)하여 지정
 LOCATION '/user/b10144880/hdfs_user'
